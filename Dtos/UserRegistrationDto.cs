1937ad9212e9baf67d28727335ccd2e4fb23d44d
Rohan Ghosh <rohan.r.ghosh@pwc.com>
2026-02-18 15:07:02 +0530

    DTO- Add UserRegistrationDto for handling user registration data transfers

---
+++ Dtos/UserRegistrationDto.cs
@@ -0,0 +1,9 @@
+namespace SocialMedia.Dtos
+{
+    public class UserRegistrationDto
+    {
+        public required string Username { get; set; }
+        public required string Password { get; set; }
+        public required string Email { get; set; }
+    }
+}
